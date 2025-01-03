be15ecbf574fdaabf6372247df3cd26f71df3e55
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-01-03 13:43:29 +0530

    Add files via upload

---
+++ Cypress/cypress/PageObjects/Pages/ADTPage.js
@@ -0,0 +1,16 @@
+class ADTPage {
+  ADT = "";
+  counters = "";
+
+  /**
+   * @Test10.3
+   * @description Initiates the patient search process in the ADT (Admission, Discharge, Transfer) page.
+   *              It creates an instance of the `PatientSearchHelper` class, clicks on the ADT dropdown,
+   *              and then calls the `searchPatient()` method from the helper to perform the actual search.
+   * @returns {void}
+   */
+  searchPatientInADT() {
+    // write your logic here
+  }
+}
+export default ADTPage;
---
+++ Cypress/cypress/PageObjects/Pages/AppointmentPage.js
@@ -0,0 +1,49 @@
+class AppointmentPage {
+  titleName = "";
+  counters = "";
+  appointment = ``;
+  selectCounter = "";
+  patientName = "";
+  searchBar = "";
+  searchResultPatientName = "";
+  hospitalSearchBar = "";
+  patientCode = "";
+  newVisitTab = ``;
+  newVisitHeading = ``;
+
+  /**
+   * @Test3
+   * @description This method searches for patients by their name and verifies that the search results
+   *              accurately match the expected patient names. It ensures that the displayed patient name 
+   *              in the search results corresponds to the searched name, confirming the search functionality 
+   *              operates correctly
+   */
+  searchAndVerifypatientName() {
+    // write your logic here
+  }
+
+  /**
+   * @Test10.1
+   * @description Initiates the patient search process in the appointment section.
+   *              It creates an instance of the `PatientSearchHelper` class, clicks on the
+   *              appointment dropdown, selects a counter, and then calls the `searchPatient()`
+   *              method from the helper to perform the actual search.
+   * @returns {void}
+   */
+  searchPatientInAppointment() {
+    // write your logic here
+  }
+
+  /**
+   * @Test12 This method opens the New Visit page in the appointment module using the Alt + N keyboard shortcut.
+   *
+   * @description This function clicks the appointment link to navigate to the appointment module.
+   *              It then clicks on the "New Visit" tab and simulates pressing the Alt + N keyboard shortcut to open
+   *              the New Visit page. After triggering the shortcut, it waits for the page to load and verifies that
+   *              the New Visit page is displayed by checking the visibility of the New Visit heading and the URL.
+   */
+  openNewVisitPageThroughKeyboardButton() {
+    // write your logic here
+  }
+}
+export default AppointmentPage;
